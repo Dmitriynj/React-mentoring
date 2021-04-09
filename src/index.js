9b284954593ba65a54dddfcfae5bb159ab4dfd83
Dzmitry Tamashevich <diaamnj@mail.ru>
2021-04-09 16:04:40 +0300

    remove redundant comment

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const preloadedState = window.__PRELOADED_STATE__;
 // Allow the passed state to be garbage-collected
 delete window.__PRELOADED_STATE__;
 
-// Create Redux store with initial state
 const store = configureStore(preloadedState);
 
 hydrate(
